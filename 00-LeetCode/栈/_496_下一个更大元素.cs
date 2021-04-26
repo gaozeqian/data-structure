0e099642fb2247b09e5ee2a975019c1e2dfc394a
gaozeqian <gaozeqian@mingtian.com>
2021-04-26 09:57:32 +0800

    496_下一个更大元素

---
+++ 00-LeetCode/栈/_496_下一个更大元素.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.栈
+{
+    /**
+     * https://leetcode-cn.com/problems/next-greater-element-i/
+     */
+    public class _496_下一个更大元素
+    {
+        public int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            var max = new int[nums1.Length];
+
+            for(int i = 0; i < nums1.Length; i++)
+            {
+                for(int j = Array.IndexOf(nums2, nums1[i]) + 1; j < nums2.Length; j++)
+                {
+                    if(nums1[i] < nums2[j])
+                    {
+                        max[i] = nums2[j];
+                        break;
+                    }
+                }
+
+                if (max[i] == 0)
+                    max[i] = -1;
+            }
+
+            return max;
+        }
+    }
+}
