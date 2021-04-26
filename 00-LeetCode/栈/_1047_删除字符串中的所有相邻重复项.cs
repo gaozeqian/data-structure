9ea6f94f737e8f1127a67ecd8fb7446680957646
gaozeqian <gaozeqian@mingtian.com>
2021-04-26 17:06:22 +0800

    1047_删除字符串中的所有相邻重复项

---
+++ 00-LeetCode/栈/_1047_删除字符串中的所有相邻重复项.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.栈
+{
+    public class _1047_删除字符串中的所有相邻重复项
+    {
+        /**
+         * https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/
+         */
+        public string RemoveDuplicates(string S)
+        {
+            var stack = new Stack<char>();
+
+            foreach(var e in S)
+            {
+                if (stack.Count > 0 && e.Equals(stack.Peek()))
+                    stack.Pop();
+                else
+                    stack.Push(e);
+            }
+
+            if (stack.Count == 0)
+                return string.Empty;
+
+            var stackReverse = new Stack<char>();
+            while (stack.Count > 0)
+                stackReverse.Push(stack.Pop());
+
+            var sb = new StringBuilder();
+            while (stackReverse.Count > 0)
+                sb.Append(stackReverse.Pop());
+
+            return sb.ToString();
+        }
+    }
+}
