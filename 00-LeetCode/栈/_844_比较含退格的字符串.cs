5c82039e5dd7818061b1ad109ef57156c6069e97
gaozeqian <gaozeqian@mingtian.com>
2021-04-26 14:50:01 +0800

    844_比较含退格的字符串

---
+++ 00-LeetCode/栈/_844_比较含退格的字符串.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.栈
+{
+    /**
+     * https://leetcode-cn.com/problems/backspace-string-compare/
+     */
+    public class _844_比较含退格的字符串
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
+            var s_stack = new Stack<char>();
+            var t_stack = new Stack<char>();
+
+            foreach (var e in s)
+            {
+                if ('#'.Equals(e))
+                {
+                    if (s_stack.Count > 0)
+                        s_stack.Pop();
+                }
+                else
+                {
+                    s_stack.Push(e);
+                }
+
+            }
+
+            foreach (var e in t)
+            {
+                if ('#'.Equals(e))
+                {
+                    if (t_stack.Count > 0)
+                        t_stack.Pop();
+                }
+                else
+                {
+                    t_stack.Push(e);
+                }
+            }
+
+            if (!s_stack.Count.Equals(t_stack.Count))
+                return false;
+
+            while (s_stack.Count > 0)
+            {
+                if (!s_stack.Pop().Equals(t_stack.Pop()))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
