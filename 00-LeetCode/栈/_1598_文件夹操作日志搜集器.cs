f5d657b060e902621951b28ab27854d3d1778528
gaozeqian <gaozeqian@mingtian.com>
2021-04-27 13:19:29 +0800

    1598_文件夹操作日志搜集器

---
+++ 00-LeetCode/栈/_1598_文件夹操作日志搜集器.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.栈
+{
+    /**
+     * https://leetcode-cn.com/problems/crawler-log-folder/
+     */
+    public class _1598_文件夹操作日志搜集器
+    {
+        public int MinOperations(string[] logs)
+        {
+            var folderStack = new Stack<string>();
+
+            foreach(var log in logs)
+            {
+                if("../".Equals(log))
+                {
+                    if (folderStack.Count > 0) folderStack.Pop();
+                }
+                else if(!"./".Equals(log))
+                {
+                    folderStack.Push(log);
+                }
+            }
+
+            return folderStack.Count;
+        }
+    }
+}
