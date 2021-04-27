254ca9ddfbcaadd20f7196c51cc0d2678a07c0d9
gaozeqian <gaozeqian@mingtian.com>
2021-04-27 17:23:17 +0800

    83_删除排序链表中的重复元素

---
+++ 00-LeetCode/链表/_83_删除排序链表中的重复元素.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.链表
+{
+    /**
+     * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+     */
+    public class _83_删除排序链表中的重复元素
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode node = head, next = node.next;
+
+            while(next != null)
+            {
+                if(node.val == next.val)
+                    node.next = next.next;
+                else
+                    node = next;
+
+                next = next.next;
+            }
+
+            return head;
+        }
+    }
+}
