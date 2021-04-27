a7034c489f9bc032d1525945079d4520b7220356
gaozeqian <gaozeqian@mingtian.com>
2021-04-27 17:26:57 +0800

    141_环形链表

---
+++ 00-LeetCode/链表/_141_环形链表.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.链表
+{
+    /**
+     * https://leetcode-cn.com/problems/linked-list-cycle/
+     */
+    public class _141_环形链表
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return false;
+
+            ListNode slow = head, fast = head.next;
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
