06f552f3f22ed877804ac0509cb9857dd9c8e572
gaozeqian <gaozeqian@mingtian.com>
2021-04-27 18:29:42 +0800

    TODO: 160_相交链表

---
+++ 00-LeetCode/链表/_160_相交链表.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _00_LeetCode.链表
+{
+    /**
+     * https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+     */
+    public class _160_相交链表
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null)
+                return null;
+
+            // TODO
+        }
+    }
+}
