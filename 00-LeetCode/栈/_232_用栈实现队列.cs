e7e35a779980006904522466e8ccb7a6603e037c
gaozeqian <gaozeqian@yeah.net>
2021-04-28 23:19:16 +0800

    优化 232_用栈实现队列

--- 00-LeetCode/栈/_232_用栈实现队列.cs
+++ 00-LeetCode/栈/_232_用栈实现队列.cs
@@ -9,75 +9,49 @@ namespace _00_LeetCode.栈
      */
     public class _232_用栈实现队列
     {
-        private readonly Stack<int> stack1;
-        private readonly Stack<int> stack2;
+        private readonly Stack<int> inStack;
+        private readonly Stack<int> outStack;
 
         /** Initialize your data structure here. */
         public _232_用栈实现队列()
         {
-            stack1 = new Stack<int>();
-            stack2 = new Stack<int>();
+            inStack = new Stack<int>();
+            outStack = new Stack<int>();
         }
 
         /** Push element x to the back of queue. */
         public void Push(int x)
         {
-            if(stack1.Count > 1)
-            {
-                while (stack1.Count > 0)
-                    stack2.Push(stack1.Pop());
-
-                stack2.Push(x);
-            }
-            else if(stack2.Count > 1)
-            {
-                while (stack2.Count > 0)
-                    stack1.Push(stack2.Pop());
-
-                stack1.Push(x);
-            }
-            else
-            {
-                if (stack1.Count == 1)
-                    stack1.Push(x);
-                else
-                    stack2.Push(x);
-            }
-
-            if(stack1.Count > 1)
-            {
-                while (stack1.Count > 0)
-                    stack2.Push(stack1.Pop());
-            }
-            else if(stack2.Count > 1)
-            {
-                while (stack2.Count > 0)
-                    stack1.Push(stack2.Pop());
-            }
+            inStack.Push(x);
         }
 
         /** Removes the element from in front of queue and returns that element. */
         public int Pop()
         {
-            if (stack1.Count > 0)
-                return stack1.Pop();
-            else
-                return stack2.Pop();
+            CheckOutStack();
+            return outStack.Pop();
         }
 
         /** Get the front element. */
         public int Peek()
         {
-            if (stack1.Count > 0)
-                return stack1.Peek();
-            else
-                return stack2.Peek();
+            CheckOutStack();
+            return outStack.Peek();
         }
 
         /** Returns whether the queue is empty. */
         public bool Empty()
         {
-            return stack1.Count == 0 && stack2.Count == 0;
+            return inStack.Count == 0 && outStack.Count == 0;
+        }
+
+        private void CheckOutStack()
+        {
+            if (outStack.Count == 0)
+            {
+                while (inStack.Count > 0)
+                    outStack.Push(inStack.Pop());
+            }
         }
     }
 
