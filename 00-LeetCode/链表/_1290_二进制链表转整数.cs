63be32d832b80f61d4e975154f008debba95ef4f
gaozeqian <gaozeqian@mingtian.com>
2021-04-28 14:11:35 +0800

    1290_二进制链表转整数

---
+++ 00-LeetCode/链表/_1290_二进制链表转整数.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.链表
+{
+    /**
+     * https://leetcode-cn.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+     */
+    public class _1290_二进制链表转整数
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            while(head != null)
+            {
+                sb.Append(head.val);
+                head = head.next;
+            }
+
+            return Convert.ToInt32(sb.ToString(), 2);
+        }
+    }
+}
