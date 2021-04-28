c40e929c935b5b447e4f5a1ea327e9e8c1a4bbf9
gaozeqian <gaozeqian@mingtian.com>
2021-04-28 09:46:04 +0800

    203_移除链表元素

---
+++ 00-LeetCode/链表/_203_移除链表元素.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.链表
+{
+    /**
+     * https://leetcode-cn.com/problems/remove-linked-list-elements/
+     */
+    public class _203_移除链表元素
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            ListNode node = head, prev = null;
+
+            while (node != null)
+            {
+                if (val == head.val)
+                {
+                    node = head = head.next;
+                }
+                else
+                {
+                    if (val == node.val)
+                    {
+                        prev.next = node.next;
+                    }
+                    else
+                    {
+                        prev = node;
+                    }
+
+                    node = node.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
