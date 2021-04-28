6bd77d44f4bb1f19dc1b0a8281708a0332e10130
gaozeqian <gaozeqian@mingtian.com>
2021-04-28 13:51:01 +0800

    234_回文链表

---
+++ 00-LeetCode/链表/_234_回文链表.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.链表
+{
+    /**
+     * https://leetcode-cn.com/problems/palindrome-linked-list/
+     */
+    public class _234_回文链表
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            var node = head;
+            var reverse = new Stack<int>();
+
+            while(node != null)
+            {
+                reverse.Push(node.val);
+                node = node.next;
+            }
+
+            while(head != null)
+            {
+                if (head.val != reverse.Pop())
+                    return false;
+
+                head = head.next;
+            }
+
+            return true;
+        }
+    }
+}
