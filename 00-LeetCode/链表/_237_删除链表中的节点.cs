ace4f7472a9c77c775658804b5f1c946b5722528
gaozeqian <gaozeqian@mingtian.com>
2021-04-28 13:57:00 +0800

    237_删除链表中的节点

---
+++ 00-LeetCode/链表/_237_删除链表中的节点.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.链表
+{
+    /**
+     * https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+     */
+    public class _237_删除链表中的节点
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
