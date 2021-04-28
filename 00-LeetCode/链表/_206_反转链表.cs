b2e98bb19fd1327d646d907778ba4df7eec64ed8
gaozeqian <gaozeqian@mingtian.com>
2021-04-28 09:52:38 +0800

    206_反转链表

---
+++ 00-LeetCode/链表/_206_反转链表.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.链表
+{
+    /**
+     * https://leetcode-cn.com/problems/reverse-linked-list/
+     */
+    public class _206_反转链表
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode newHead = null;
+            while (head != null)
+            {
+                var tmp = head.next;
+                head.next = newHead;
+                newHead = head;
+                head = tmp;
+            }
+
+            return newHead;
+        }
+    }
+}
