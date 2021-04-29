9ba50068edbe18e4aaeeee2a6e62b9c4ab9f60d7
gaozeqian <gaozeqian@mingtian.com>
2021-04-29 11:40:16 +0800

    35_搜索插入位置

---
+++ 00-LeetCode/数组/_35_搜索插入位置.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /**
+     * https://leetcode-cn.com/problems/search-insert-position/
+     */
+    public class _35_搜索插入位置
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (target <= nums[i])
+                    return i;
+            }
+
+            return nums.Length;
+        }
+    }
+}
