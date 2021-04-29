6994c09f6895a030a0e24b2b6ce2e749a8728f53
gaozeqian <gaozeqian@mingtian.com>
2021-04-29 11:06:45 +0800

    26_删除有序数组中的重复项

---
+++ 00-LeetCode/数组/_26_删除有序数组中的重复项.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /**
+     * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+     */
+    public class _26_删除有序数组中的重复项
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length <= 1)
+                return nums.Length;
+
+            int length = nums.Length;
+
+            for (int i = 1; i < length;)
+            {
+                if (nums[i - 1] == nums[i])
+                {
+                    length--;
+
+                    for (int j = i; j < length;)
+                    {
+                        nums[j] = nums[++j];
+                    }
+
+                    continue;
+                }
+
+                i++;
+            }
+
+            return length;
+        }
+    }
+}
