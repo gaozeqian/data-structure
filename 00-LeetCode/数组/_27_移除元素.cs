f5733b7edb98bf1d61066b74c867c1c991dfd4f6
gaozeqian <gaozeqian@mingtian.com>
2021-04-29 11:29:41 +0800

    27_移除元素

---
+++ 00-LeetCode/数组/_27_移除元素.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /**
+     * https://leetcode-cn.com/problems/remove-element/
+     */
+    public class _27_移除元素
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            int length = nums.Length;
+            
+            for(int i = 0; i < length;)
+            {
+                if(val == nums[i])
+                {
+                    length--;
+
+                    for(int j = i; j < length;)
+                    {
+                        nums[j] = nums[++j];
+                    }
+
+                    continue;
+                }
+
+                i++;
+            }
+
+            return length;
+        }
+    }
+}
