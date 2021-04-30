96c2c75e7572be10252d161dbd2bec01135d2373
gaozeqian <gaozeqian@mingtian.com>
2021-04-30 18:17:50 +0800

    TODO: 217_存在重复元素

---
+++ 00-LeetCode/数组/_217_存在重复元素.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _00_LeetCode.数组
+{
+    /**
+     * https://leetcode-cn.com/problems/contains-duplicate/
+     */
+    public class _217_存在重复元素
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            //TODO
+            return false;
+        }
+    }
+}
