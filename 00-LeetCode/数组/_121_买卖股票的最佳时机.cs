360683b4bcf8e7cda2cbc27541cba73d11f9a369
gaozeqian <gaozeqian@mingtian.com>
2021-04-30 16:08:42 +0800

    121_买卖股票的最佳时机

---
+++ 00-LeetCode/数组/_121_买卖股票的最佳时机.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /**
+     * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+     */
+    public class _121_买卖股票的最佳时机
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue, 
+                maxProfit = 0;
+
+            foreach(var price in prices)
+            {
+                if(price < minPrice)
+                {
+                    minPrice = price;
+                }
+                else if(price - minPrice > maxProfit)
+                {
+                    maxProfit = price - minPrice;
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
