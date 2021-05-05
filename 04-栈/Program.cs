985aef5d82ad64fdd2cfb647ad18293bcb90724c
gaozeqian <gaozeqian@yeah.net>
2021-05-05 17:15:52 +0800

    04-栈

---
+++ 04-栈/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04_栈
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
