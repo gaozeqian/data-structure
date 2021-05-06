70f2f13517c4a11458b389e071b96718cb1b2219
gaozeqian <gaozeqian@mingtian.com>
2021-05-06 13:39:15 +0800

    509_斐波那契数

---
+++ 00-LeetCode/数组/_509_斐波那契数.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/fibonacci-number/
+    /// </summary>
+    public class _509_斐波那契数
+    {
+        public int Fib(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+            {
+                int zero = 0, first = 1;
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    int sum = zero + first;
+                    zero = first;
+                    first = sum;
+                }
+
+                return first;
+            }
+        }
+    }
+}
