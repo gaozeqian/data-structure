66b970173990b3e282fca0916c07280ac0163bbd
gaozeqian <gaozeqian@mingtian.com>
2021-05-06 13:27:21 +0800

    485_最大连续_1_的个数

---
+++ 00-LeetCode/数组/_485_最大连续 1 的个数.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/max-consecutive-ones/
+    /// </summary>
+    public class _485_最大连续_1_的个数
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int counter = 0, 
+                tmp = 0;
+
+            foreach(var num in nums)
+            {
+                if (num == 1)
+                    tmp++;
+                else
+                {
+                    if (tmp > counter)
+                        counter = tmp;
+
+                    tmp = 0;
+                }
+            }
+
+            if (tmp > counter)
+                counter = tmp;
+
+            return counter;
+        }
+    }
+}
