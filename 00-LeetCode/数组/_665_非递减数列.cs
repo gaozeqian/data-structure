ab6258222d54c756eec72dcb515267e56b6c437b
gaozeqian <gaozeqian@mingtian.com>
2021-05-07 16:53:34 +0800

    TODO

---
+++ 00-LeetCode/数组/_665_非递减数列.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/non-decreasing-array/
+    /// </summary>
+    public class _665_非递减数列
+    {
+        public bool CheckPossibility(int[] nums)
+        {
+            // TODO
+            return false;
+        }
+    }
+}
