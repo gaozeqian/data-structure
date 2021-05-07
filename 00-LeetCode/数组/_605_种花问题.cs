61ed2aaadd71b4ae0f5791b88737001de78ad8ee
gaozeqian <gaozeqian@mingtian.com>
2021-05-07 09:47:07 +0800

    605_种花问题

---
+++ 00-LeetCode/数组/_605_种花问题.cs
@@ -0,0 +1,40 @@
+﻿namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/can-place-flowers/
+    /// </summary>
+    public class _605_种花问题
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            if (n == 0)
+                return true;
+
+            int counter = 1;
+            for(int i = 0; i < flowerbed.Length; i++)
+            {
+                if(counter == 3)
+                {
+                    n--;
+                    counter = 1;
+                }
+
+                if (n == 0)
+                    return true;
+
+                if (flowerbed[i] == 0)
+                    counter++;
+                else
+                    counter = 0;
+            }
+
+            if (counter >= 2)
+                n--;
+
+            if (n == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
