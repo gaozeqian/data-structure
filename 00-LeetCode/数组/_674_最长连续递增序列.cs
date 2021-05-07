fbd19704b0c1c5721780b5a25d8e1346e8365224
gaozeqian <gaozeqian@mingtian.com>
2021-05-07 17:09:57 +0800

    674_最长连续递增序列

---
+++ 00-LeetCode/数组/_674_最长连续递增序列.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/longest-continuous-increasing-subsequence/
+    /// </summary>
+    public class _674_最长连续递增序列
+    {
+        public int FindLengthOfLCIS(int[] nums)
+        {
+            if (nums.Length <= 1)
+                return nums.Length;
+
+            int maxLength = 1, tmpLength = 1;
+            for(int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                    tmpLength++;
+                else
+                {
+                    if (tmpLength > maxLength)
+                        maxLength = tmpLength;
+
+                    tmpLength = 1;
+                }
+            }
+
+            return Math.Max(maxLength, tmpLength);
+        }
+    }
+}
