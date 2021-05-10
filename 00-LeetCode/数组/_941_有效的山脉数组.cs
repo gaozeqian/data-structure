97f6cef79ce4992a22f150baf0bb8a7d56d2b6bc
gaozeqian <gaozeqian@mingtian.com>
2021-05-10 16:12:32 +0800

    941_有效的山脉数组

---
+++ 00-LeetCode/数组/_941_有效的山脉数组.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/valid-mountain-array/
+    /// </summary>
+    public class _941_有效的山脉数组
+    {
+        public bool ValidMountainArray(int[] arr)
+        {
+            if (arr.Length < 3)
+                return false;
+
+            bool isIncrement = true;
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1] == arr[i])
+                    return false;
+
+                if (isIncrement && (arr[i - 1] > arr[i]))
+                    isIncrement = false;
+
+                if (!isIncrement && (arr[i - 1] < arr[i]))
+                    return false;
+            }
+
+            return !isIncrement && (arr[0] < arr[1]);
+        }
+    }
+}
