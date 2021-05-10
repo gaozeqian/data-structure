f2d9f5c9b057030d5010f5c82adfb3ba612bd284
gaozeqian <gaozeqian@mingtian.com>
2021-05-10 11:06:16 +0800

    905_按奇偶排序数组

---
+++ 00-LeetCode/数组/_905_按奇偶排序数组.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/sort-array-by-parity/
+    /// </summary>
+    public class _905_按奇偶排序数组
+    {
+        public int[] SortArrayByParity(int[] A)
+        {
+            if (A.Length == 1)
+                return A;
+
+            List<int> even = new List<int>(A.Length);
+            List<int> odd = new List<int>(A.Length);
+
+            foreach(var num in A)
+            {
+                if ((num & 1) == 0)
+                    even.Add(num);
+                else
+                    odd.Add(num);
+            }
+
+            even.AddRange(odd);
+            return even.ToArray();
+        }
+    }
+}
