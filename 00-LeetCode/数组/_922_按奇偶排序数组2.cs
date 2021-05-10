13d8a5192ad7e0d102c08c2260fd97addc7c19be
gaozeqian <gaozeqian@mingtian.com>
2021-05-10 15:13:02 +0800

    922_按奇偶排序数组2

---
+++ 00-LeetCode/数组/_922_按奇偶排序数组2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/sort-array-by-parity-ii/
+    /// </summary>
+    public class _922_按奇偶排序数组2
+    {
+        public int[] SortArrayByParityII(int[] nums)
+        {
+            int[] sortedNums = new int[nums.Length];
+            int evenIndex = 0, oddIndex = 1;
+
+            foreach(int num in nums)
+            {
+                if ((num & 1) == 0)
+                {
+                    sortedNums[evenIndex] = num;
+                    evenIndex += 2;
+                }
+                else
+                {
+                    sortedNums[oddIndex] = num;
+                    oddIndex += 2;
+                }
+            }
+
+            return sortedNums;
+        }
+    }
+}
