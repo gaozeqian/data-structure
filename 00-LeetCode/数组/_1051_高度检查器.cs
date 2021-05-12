4abf7248e571da1b62a00070ed3015430a689d14
gaozeqian <gaozeqian@mingtian.com>
2021-05-12 18:19:57 +0800

    1051_高度检查器

---
+++ 00-LeetCode/数组/_1051_高度检查器.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/height-checker/
+    /// </summary>
+    public class _1051_高度检查器
+    {
+        public int HeightChecker(int[] heights)
+        {
+            if (heights.Length == 1)
+                return 0;
+
+            int[] original = new int[heights.Length];
+            Array.Copy(heights, original, heights.Length);
+            Array.Sort(heights);
+
+            int counter = 0;
+            for(int i = 0; i < heights.Length; i++)
+            {
+                if (heights[i] != original[i])
+                    counter++;
+            }
+
+            return counter++;
+        }
+    }
+}
