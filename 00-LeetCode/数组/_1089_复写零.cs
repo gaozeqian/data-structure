4d8485f07ae706c4e43ed693c43d12f1efdbe357
gaozeqian <gaozeqian@mingtian.com>
2021-05-13 10:35:43 +0800

    1089_复写零

---
+++ 00-LeetCode/数组/_1089_复写零.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/duplicate-zeros/
+    /// </summary>
+    public class _1089_复写零
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+            int[] answer = new int[arr.Length];
+            for (int i = 0, j = 0; i < arr.Length && j < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                    j += 2;
+                else
+                    answer[j++] = arr[i];
+            }
+            Array.Copy(answer, arr, arr.Length);
+        }
+    }
+}
