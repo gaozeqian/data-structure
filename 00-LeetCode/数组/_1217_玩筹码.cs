ccdb726e9a36bfa3c10ebfa6003e1c8584a5ea3c
gaozeqian <gaozeqian@mingtian.com>
2021-05-14 17:03:07 +0800

    1217_玩筹码

---
+++ 00-LeetCode/数组/_1217_玩筹码.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+    /// </summary>
+    public class _1217_玩筹码
+    {
+        public int MinCostToMoveChips(int[] position)
+        {
+            int[] chips = new int[2];
+            foreach(int pos in position)
+            {
+                if ((pos & 1) == 0)
+                    chips[0]++;
+                else
+                    chips[1]++;
+            }
+
+            return Math.Min(chips[0], chips[1]);
+        }
+    }
+}
