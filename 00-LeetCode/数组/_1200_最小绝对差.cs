e78f53d5dc8bad68b48251a92a6c8a04222f1c64
gaozeqian <gaozeqian@mingtian.com>
2021-05-14 16:36:54 +0800

    1200_最小绝对差

---
+++ 00-LeetCode/数组/_1200_最小绝对差.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/minimum-absolute-difference/
+    /// </summary>
+    public class _1200_最小绝对差
+    {
+        public IList<IList<int>> MinimumAbsDifference(int[] arr)
+        {
+            var answer = new List<IList<int>>();
+            Array.Sort(arr);
+
+            int minMinus = int.MaxValue;
+            for(int i = 1; i < arr.Length; i++)
+            {
+                int tmp = arr[i] - arr[i - 1];
+                if(tmp < minMinus)
+                {
+                    minMinus = tmp;
+                    answer.Clear();
+                    answer.Add(new List<int> { arr[i - 1], arr[i] });
+                }
+                else if(tmp == minMinus)
+                {
+                    answer.Add(new List<int> { arr[i - 1], arr[i] });
+                }
+            }
+
+            return answer;
+        }
+    }
+}
