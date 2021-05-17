c0ca057ea5fc3b00145fee14bd85a39f63f9d02c
gaozeqian <gaozeqian@mingtian.com>
2021-05-17 15:08:49 +0800

    1266_访问所有点的最小时间

---
+++ 00-LeetCode/数组/_1266_访问所有点的最小时间.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/minimum-time-visiting-all-points/
+    /// </summary>
+    public class _1266_访问所有点的最小时间
+    {
+        public int MinTimeToVisitAllPoints(int[][] points)
+        {
+            int minTime = 0;
+            for(int i = 1; i < points.Length; i++)
+            {
+                minTime += Math.Max(Math.Abs(points[i][0] - points[i - 1][0]), Math.Abs(points[i][1] - points[i - 1][1]));
+            }
+
+            return minTime;
+        }
+    }
+}
