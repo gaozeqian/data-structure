edc7397431990f458008cfac77a39436cf705a17
gaozeqian <gaozeqian@mingtian.com>
2021-05-18 17:33:48 +0800

    1346_检查整数及其两倍数是否存在

---
+++ 00-LeetCode/数组/_1346_检查整数及其两倍数是否存在.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/check-if-n-and-its-double-exist/
+    /// </summary>
+    public class _1346_检查整数及其两倍数是否存在
+    {
+        public bool CheckIfExist(int[] arr)
+        {
+            HashSet<int> set = new HashSet<int>();
+            HashSet<int> doubleSet = new HashSet<int>();
+            foreach(int num in arr)
+            {
+                if (set.Contains(num << 1) || doubleSet.Contains(num))
+                    return true;
+                else
+                {
+                    set.Add(num);
+                    doubleSet.Add(num << 1);
+                }
+            }
+
+            return false;
+        }
+    }
+}
