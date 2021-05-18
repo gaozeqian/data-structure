9db37e2ef5352595c6eee824acf6dc8a681fe4eb
gaozeqian <gaozeqian@mingtian.com>
2021-05-18 10:27:14 +0800

    1304_和为零的N个唯一整数

---
+++ 00-LeetCode/数组/_1304_和为零的N个唯一整数.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/find-n-unique-integers-sum-up-to-zero/
+    /// </summary>
+    public class _1304_和为零的N个唯一整数
+    {
+        public int[] SumZero(int n)
+        {
+            int[] answer = new int[n];
+            int sum = 0;
+            for(int i = 1; i < n; i++)
+            {
+                answer[i] = i;
+                sum += i;
+            }
+            answer[0] = -sum;
+
+            return answer;
+        }
+    }
+}
