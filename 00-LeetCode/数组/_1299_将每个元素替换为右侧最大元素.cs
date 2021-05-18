1ec6311460ccf812933ba09c4a588e3fb9112faa
gaozeqian <gaozeqian@mingtian.com>
2021-05-18 10:08:46 +0800

    1299_将每个元素替换为右侧最大元素

---
+++ 00-LeetCode/数组/_1299_将每个元素替换为右侧最大元素.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/replace-elements-with-greatest-element-on-right-side/
+    /// </summary>
+    public class _1299_将每个元素替换为右侧最大元素
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            int rightMax = arr[arr.Length - 1];
+            arr[arr.Length - 1] = -1;
+
+            for(int i = arr.Length - 2; i >= 0; i--)
+            {
+                if (arr[i] <= rightMax)
+                    arr[i] = rightMax;
+                else
+                {
+                    int max = arr[i];
+                    arr[i] = rightMax;
+                    rightMax = max;
+                }
+            }
+
+            return arr;
+        }
+    }
+}
