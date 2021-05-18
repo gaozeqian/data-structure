df5b8ee57ff267c68ab3c7c2ec5c7c9aa3994adb
gaozeqian <gaozeqian@mingtian.com>
2021-05-18 09:52:08 +0800

    1295_统计位数为偶数的数字

---
+++ 00-LeetCode/数组/_1295_统计位数为偶数的数字.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/find-numbers-with-even-number-of-digits/
+    /// </summary>
+    public class _1295_统计位数为偶数的数字
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int answer = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int tmp = 0;
+                while(nums[i] != 0)
+                {
+                    tmp++;
+                    nums[i] /= 10;
+                }
+
+                if ((tmp & 1) == 0)
+                    answer++;
+            }
+
+            return answer;
+        }
+    }
+}
