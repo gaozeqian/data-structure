668b665dc0054ef4cfbeb9b934ea036fde4f98ea
gaozeqian <gaozeqian@mingtian.com>
2021-05-19 16:05:50 +0800

    1464_数组中两元素的最大乘积

---
+++ 00-LeetCode/数组/_1464_数组中两元素的最大乘积.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/maximum-product-of-two-elements-in-an-array/
+    /// </summary>
+    public class _1464_数组中两元素的最大乘积
+    {
+        public int MaxProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            return (nums[nums.Length - 1] - 1) * (nums[nums.Length - 2] - 1);
+        }
+    }
+}
