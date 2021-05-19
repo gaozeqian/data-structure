2a639d8f2c37c1454bd31a5eb9343a023a44cbbd
gaozeqian <gaozeqian@mingtian.com>
2021-05-19 13:08:40 +0800

    1413_逐步求和得到正数的最小值

---
+++ 00-LeetCode/数组/_1413_逐步求和得到正数的最小值.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/minimum-value-to-get-positive-step-by-step-sum/
+    /// </summary>
+    public class _1413_逐步求和得到正数的最小值
+    {
+        public int MinStartValue(int[] nums)
+        {
+            int minSum = int.MaxValue;
+            int sum = 0;
+            foreach(int num in nums)
+            {
+                sum += num;
+                if (sum < minSum)
+                    minSum = sum;
+            }
+
+            return minSum >= 0 ? 1 : 1 - minSum;
+        }
+    }
+}
