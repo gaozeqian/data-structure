67e26e8b1f07e6ac1d8522642f3cbe8ca83525ea
gaozeqian <gaozeqian@mingtian.com>
2021-05-19 14:12:55 +0800

    1450_在既定时间做作业的学生人数

---
+++ 00-LeetCode/数组/_1450_在既定时间做作业的学生人数.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/number-of-students-doing-homework-at-a-given-time/
+    /// </summary>
+    public class _1450_在既定时间做作业的学生人数
+    {
+        public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+        {
+            int students = 0;
+            for(int i = 0; i < endTime.Length; i++)
+            {
+                if (queryTime >= startTime[i] && queryTime <= endTime[i])
+                    students++;
+            }
+
+            return students;
+        }
+    }
+}
