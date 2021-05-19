0b7ec17e3fed38a6c58c982a9c01befef884dcdc
gaozeqian <gaozeqian@mingtian.com>
2021-05-19 14:04:31 +0800

    1437_是否所有1都至少相隔k个元素

---
+++ 00-LeetCode/数组/_1437_是否所有1都至少相隔k个元素.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+    /// </summary>
+    public class _1437_是否所有1都至少相隔k个元素
+    {
+        public bool KLengthApart(int[] nums, int k)
+        {
+            int places = -1;
+            foreach(int num in nums)
+            {
+                if(num == 1)
+                {
+                    if (places >= 0 && places < k)
+                        return false;
+
+                    places = 0;
+                }
+                else if(places >= 0)
+                {
+                    places++;
+                }
+            }
+
+            return true;
+        }
+    }
+}
