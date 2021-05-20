a15bb5c41309d7c46a5fd9e0b791679d67c9fa6b
gaozeqian <gaozeqian@mingtian.com>
2021-05-20 16:30:09 +0800

    1534_统计好三元组

---
+++ 00-LeetCode/数组/_1534_统计好三元组.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/count-good-triplets/
+    /// </summary>
+    public class _1534_统计好三元组
+    {
+        public int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            int answer = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for(int j = i + 1; j < arr.Length; j++)
+                {
+                    for(int k = j + 1; k < arr.Length; k++)
+                    {
+                        if (Math.Abs(arr[i] - arr[j]) <= a && Math.Abs(arr[j] - arr[k]) <= b && Math.Abs(arr[i] - arr[k]) <= c)
+                            answer++;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
