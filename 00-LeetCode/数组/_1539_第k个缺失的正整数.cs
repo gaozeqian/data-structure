b7128fb3105c28db468703c7b00a9b6355172c61
gaozeqian <gaozeqian@mingtian.com>
2021-05-20 16:51:13 +0800

    1539_第k个缺失的正整数

---
+++ 00-LeetCode/数组/_1539_第k个缺失的正整数.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/kth-missing-positive-number/
+    /// </summary>
+    public class _1539_第k个缺失的正整数
+    {
+        public int FindKthPositive(int[] arr, int k)
+        {
+            int[] count = new int[2001];
+            foreach (int num in arr)
+                count[num] = 1;
+
+            for(int i = 1; i < 2001; i++)
+            {
+                if (count[i] == 0 && --k == 0)
+                    return i;
+            }
+
+            return 0;
+        }
+    }
+}
