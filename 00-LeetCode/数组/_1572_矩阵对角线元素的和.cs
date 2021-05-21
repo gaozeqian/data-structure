6e58f4ae44d0677bf6610b8155ddc440005ec1fd
gaozeqian <gaozeqian@mingtian.com>
2021-05-21 11:31:45 +0800

    1572_矩阵对角线元素的和

---
+++ 00-LeetCode/数组/_1572_矩阵对角线元素的和.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/matrix-diagonal-sum/
+    /// </summary>
+    public class _1572_矩阵对角线元素的和
+    {
+        public int DiagonalSum(int[][] mat)
+        {
+            int sum = 0;
+            int except = 0;
+
+            for(int r = 0, c = 0; r < mat.Length; r++, c++)
+            {
+                sum += mat[r][c];
+            }
+
+            for(int r = 0, c = mat.Length - 1; r < mat.Length; r++, c--)
+            {
+                sum += mat[r][c];
+                if (r == c)
+                    except = mat[r][c];
+            }
+
+            return sum - except;
+        }
+    }
+}
