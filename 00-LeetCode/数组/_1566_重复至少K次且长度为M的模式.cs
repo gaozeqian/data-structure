ea6af24670f028d910cc26639ff478d87337d5bd
gaozeqian <gaozeqian@mingtian.com>
2021-05-21 10:54:55 +0800

    1566_重复至少K次且长度为M的模式

---
+++ 00-LeetCode/数组/_1566_重复至少K次且长度为M的模式.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/detect-pattern-of-length-m-repeated-k-or-more-times/
+    /// </summary>
+    public class _1566_重复至少K次且长度为M的模式
+    {
+        public bool ContainsPattern(int[] arr, int m, int k)
+        {
+            for (int start = 0; start <= arr.Length - m * k; start++)
+            {
+                int offset;
+                for (offset = 0; offset < m * k; offset++)
+                {
+                    if (arr[start + offset] != arr[start + offset % m])
+                        break;
+                }
+
+                if (offset == m * k)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
