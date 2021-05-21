8c234d25dcd4ea1e58565d682449f8652eac9c42
gaozeqian <gaozeqian@mingtian.com>
2021-05-21 17:53:41 +0800

    1619_删除某些元素后的数组均值

---
+++ 00-LeetCode/数组/_1619_删除某些元素后的数组均值.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/mean-of-array-after-removing-some-elements/
+    /// </summary>
+    public class _1619_删除某些元素后的数组均值
+    {
+        public double TrimMean(int[] arr)
+        {
+            Array.Sort(arr);
+
+            int start = arr.Length * 5 / 100;
+            int end = arr.Length - start;
+            int sum = 0;
+            for (int i = start; i < end; i++)
+                sum += arr[i];
+
+            return sum / (double)(end - start);
+        }
+    }
+}
