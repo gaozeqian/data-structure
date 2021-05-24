c552e4332d778a53828d82aa871b986e44b35ba6
gaozeqian <gaozeqian@mingtian.com>
2021-05-24 18:12:38 +0800

    1732_找到最高海拔

---
+++ 00-LeetCode/数组/_1732_找到最高海拔.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/find-the-highest-altitude/
+    /// </summary>
+    public class _1732_找到最高海拔
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int max = int.MinValue;
+            int sum = 0;
+            for(int i = 0; i < gain.Length; i++)
+            {
+                sum += gain[i];
+                if (sum > max)
+                    max = sum;
+            }
+
+            return Math.Max(0, max);
+        }
+    }
+}
