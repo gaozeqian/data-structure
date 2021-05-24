4d56392017d0342c8cfb2ee6222608f7c8ccab9a
gaozeqian <gaozeqian@mingtian.com>
2021-05-24 16:38:53 +0800

    1672_最富有客户的资产总量

---
+++ 00-LeetCode/数组/_1672_最富有客户的资产总量.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/richest-customer-wealth/
+    /// </summary>
+    public class _1672_最富有客户的资产总量
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int max = 0;
+            for(int r = 0; r < accounts.Length; r++)
+            {
+                int sum = 0;
+                for (int c = 0; c < accounts[r].Length; c++)
+                    sum += accounts[r][c];
+
+                if (sum > max)
+                    max = sum;
+            }
+
+            return max;
+        }
+    }
+}
