2476f3d8d436e94000f1eab9cab9919ac16bdf73
gaozeqian <gaozeqian@mingtian.com>
2021-05-24 15:12:45 +0800

    1646_获取生成数组中的最大值

---
+++ 00-LeetCode/数组/_1646_获取生成数组中的最大值.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/get-maximum-in-generated-array/
+    /// </summary>
+    public class _1646_获取生成数组中的最大值
+    {
+        public int GetMaximumGenerated(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            int[] nums = new int[n + 1];
+            nums[1] = 1;
+            int max = 0;
+            for(int i = 2; i <= n; i++)
+            {
+                if ((i & 1) == 0)
+                    nums[i] = nums[i >> 1];
+                else
+                    nums[i] = nums[(i - 1) >> 1] + nums[((i - 1) >> 1) + 1];
+
+                if (nums[i] > max)
+                    max = nums[i];
+            }
+
+            return max;
+        }
+    }
+}
