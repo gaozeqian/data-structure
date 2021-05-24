4312a6180216b4b3cd44d449ba5d22e94d941082
gaozeqian <gaozeqian@mingtian.com>
2021-05-24 13:55:46 +0800

    1636_按照频率将数组升序排序

---
+++ 00-LeetCode/数组/_1636_按照频率将数组升序排序.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/sort-array-by-increasing-frequency/
+    /// </summary>
+    public class _1636_按照频率将数组升序排序
+    {
+        public int[] FrequencySort(int[] nums)
+        {
+            int[] freq = new int[201];
+            foreach (int num in nums)
+                freq[num + 100]++;
+
+            int min = 201;
+            int max = 1;
+            foreach(int num in freq)
+            {
+                if (num < min)
+                    min = num;
+
+                if (num > max)
+                    max = num;
+            }
+
+            int[] ans = new int[nums.Length];
+            int index = 0;
+            for(int i = min; i <= max; i++)
+            {
+                for(int j = 200; j >= 0; j--)
+                {
+                    if(freq[j] == i)
+                    {
+                        for (int k = 0; k < i; k++)
+                            ans[index++] = j - 100;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
