0b121c307bae9c83fd8b34278b3d8f0ca9b4607e
gaozeqian <gaozeqian@mingtian.com>
2021-05-25 11:39:24 +0800

    1748_唯一元素的和

---
+++ 00-LeetCode/数组/_1748_唯一元素的和.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/sum-of-unique-elements/
+    /// </summary>
+    public class _1748_唯一元素的和
+    {
+        public int SumOfUnique(int[] nums)
+        {
+            int[] count = new int[101];
+            foreach (int num in nums)
+                count[num]++;
+
+            int sum = 0;
+            for(int i = 1; i < 101; i++)
+            {
+                if (count[i] == 1)
+                    sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
