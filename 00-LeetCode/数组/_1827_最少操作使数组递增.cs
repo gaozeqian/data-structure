a5eb7eee1bdd815b01594764f879a77c75b4d818
gaozeqian <gaozeqian@mingtian.com>
2021-05-25 18:08:33 +0800

    1827_最少操作使数组递增

---
+++ 00-LeetCode/数组/_1827_最少操作使数组递增.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/minimum-operations-to-make-the-array-increasing/
+    /// </summary>
+    public class _1827_最少操作使数组递增
+    {
+        public int MinOperations(int[] nums)
+        {
+            int count = 0;
+            for(int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] <= nums[i - 1])
+                {
+                    int newVal = nums[i - 1] + 1;
+                    count += newVal - nums[i];
+                    nums[i] = newVal;
+                }
+            }
+
+            return count;
+        }
+    }
+}
