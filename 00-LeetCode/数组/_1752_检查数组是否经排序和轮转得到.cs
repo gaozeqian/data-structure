a185168a2a483bb347b6e86b3b6138b6238c0c73
gaozeqian <gaozeqian@mingtian.com>
2021-05-25 15:12:07 +0800

    1752_检查数组是否经排序和轮转得到

---
+++ 00-LeetCode/数组/_1752_检查数组是否经排序和轮转得到.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/check-if-array-is-sorted-and-rotated/
+    /// </summary>
+    public class _1752_检查数组是否经排序和轮转得到
+    {
+        public bool Check(int[] nums)
+        {
+            int count = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[(i + 1) % nums.Length])
+                    count++;
+            }
+
+            return count <= 1;
+        }
+    }
+}
