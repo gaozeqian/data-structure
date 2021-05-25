b85a7ef5c6aab2e1dee94f5c7fda8cf5e246b650
gaozeqian <gaozeqian@mingtian.com>
2021-05-25 16:20:35 +0800

    1773_统计匹配检索规则的物品数量

---
+++ 00-LeetCode/数组/_1773_统计匹配检索规则的物品数量.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/count-items-matching-a-rule/
+    /// </summary>
+    public class _1773_统计匹配检索规则的物品数量
+    {
+        public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            return items.Where(
+                item => ("type".Equals(ruleKey) && item[0].Equals(ruleValue)) || 
+                ("color".Equals(ruleKey) && item[1].Equals(ruleValue)) || 
+                ("name".Equals(ruleKey) && item[2].Equals(ruleValue))).Count();
+        }
+    }
+}
