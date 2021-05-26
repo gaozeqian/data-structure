adbb7e51277121b831d3376ee2b4e2cdadadf3af
gaozeqian <gaozeqian@mingtian.com>
2021-05-26 15:07:03 +0800

    268_丢失的数字

---
+++ 00-LeetCode/位运算/_268_丢失的数字.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/missing-number/
+    /// </summary>
+    public class _268_丢失的数字
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int ans = nums.Length;
+            for (int i = 0; i < nums.Length; i++)
+                ans ^= i ^ nums[i];
+            return ans;
+        }
+    }
+}
