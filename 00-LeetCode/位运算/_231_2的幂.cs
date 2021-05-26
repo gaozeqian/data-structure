a3b26ac8b333c45e04e0da1db343a250f0b3202f
gaozeqian <gaozeqian@mingtian.com>
2021-05-26 14:53:52 +0800

    231_2的幂

---
+++ 00-LeetCode/位运算/_231_2的幂.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/power-of-two/
+    /// </summary>
+    public class _231_2的幂
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            return n > 0 && (n & (n - 1)) == 0;
+        }
+    }
+}
