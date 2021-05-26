e014788454d7c9954019065a2fc55a5218583238
gaozeqian <gaozeqian@mingtian.com>
2021-05-26 13:59:14 +0800

    136_只出现一次的数字

---
+++ 00-LeetCode/位运算/_136_只出现一次的数字.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/single-number/
+    /// </summary>
+    public class _136_只出现一次的数字
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int single = 0;
+            foreach (int num in nums)
+                single ^= num;
+            return single;
+        }
+    }
+}
