1d2394d704d1d7ad3f491e32d86c2bd28ff977c0
gaozeqian <gaozeqian@mingtian.com>
2021-05-26 10:02:23 +0800

    1848_到目标元素的最小距离

---
+++ 00-LeetCode/数组/_1848_到目标元素的最小距离.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.数组
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/minimum-distance-to-the-target-element/
+    /// </summary>
+    public class _1848_到目标元素的最小距离
+    {
+        public int GetMinDistance(int[] nums, int target, int start)
+        {
+            int min = int.MaxValue;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == target)
+                {
+                    int abs = Math.Abs(i - start);
+                    if (abs < min)
+                        min = abs;
+                }
+            }
+
+            return min;
+        }
+    }
+}
