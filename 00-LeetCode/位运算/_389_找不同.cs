9e182db92ff042a4c8d9f5e3b5980a2aa7f115fc
gaozeqian <gaozeqian@mingtian.com>
2021-05-28 14:35:25 +0800

    389_找不同

---
+++ 00-LeetCode/位运算/_389_找不同.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/find-the-difference/
+    /// </summary>
+    public class _389_找不同
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            int ans = 0;
+
+            foreach (char chr in s)
+                ans ^= chr;
+
+            foreach (char chr in t)
+                ans ^= chr;
+
+            return (char)ans;
+        }
+    }
+}
