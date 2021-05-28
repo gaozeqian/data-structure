2284f1c21f8bf8213307cef666e13fd68a9fa7d1
gaozeqian <gaozeqian@mingtian.com>
2021-05-28 17:55:38 +0800

    1342_将数字变成0的操作次数

---
+++ 00-LeetCode/位运算/_1342_将数字变成0的操作次数.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+    /// </summary>
+    public class _1342_将数字变成0的操作次数
+    {
+        public int NumberOfSteps(int num)
+        {
+            int ans = 0;
+            while(num > 0)
+            {
+                ans++;
+
+                if ((num & 1) == 0)
+                    num /= 2;
+                else
+                    num -= 1;
+            }
+
+            return ans;
+        }
+    }
+}
