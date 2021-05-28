0f13593085e494658ecf7dc7c360ffcb255e8f78
gaozeqian <gaozeqian@mingtian.com>
2021-05-28 16:45:29 +0800

    461_汉明距离

---
+++ 00-LeetCode/位运算/_461_汉明距离.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/hamming-distance/
+    /// </summary>
+    public class _461_汉明距离
+    {
+        public int HammingDistance(int x, int y)
+        {
+            x ^= y;
+            int ans = 0;
+            while(x != 0)
+            {
+                ans++;
+                x &= x - 1;
+            }
+
+            return ans;
+        }
+    }
+}
