e7afedeb685fa71f44144dceabaef5944baabb4e
gaozeqian <gaozeqian@mingtian.com>
2021-05-28 14:20:07 +0800

    342_4的幂

---
+++ 00-LeetCode/位运算/_342_4的幂.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/power-of-four/
+    /// </summary>
+    public class _342_4的幂
+    {
+        public bool IsPowerOfFour(int n)
+        {
+            return n > 0 && (n & (n - 1)) == 0 && (n & 0xaaaaaaaa) == 0;
+        }
+    }
+}
