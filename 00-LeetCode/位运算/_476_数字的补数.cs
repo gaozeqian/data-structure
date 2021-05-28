99a7c13832acf0abf16da22d565601c1ca68c332
gaozeqian <gaozeqian@mingtian.com>
2021-05-28 16:55:09 +0800

    476_数字的补数

---
+++ 00-LeetCode/位运算/_476_数字的补数.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/number-complement/
+    /// </summary>
+    public class _476_数字的补数
+    {
+        public int FindComplement(int num)
+        {
+            int ans = 0;
+            for(int i = 0; num != 0; i++)
+            {
+                ans += ((num & 1) ^ 1) << i;
+                num >>= 1;
+            }
+
+            return ans;
+        }
+    }
+}
