e50ab52ed23113214b84dc1ae004eee6e16f6505
gaozeqian <gaozeqian@mingtian.com>
2021-05-28 16:33:51 +0800

    405_数字转换为十六进制数

---
+++ 00-LeetCode/位运算/_405_数字转换为十六进制数.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/convert-a-number-to-hexadecimal/
+    /// </summary>
+    public class _405_数字转换为十六进制数
+    {
+        public string ToHex(int num)
+        {
+            if (num == 0)
+                return "0";
+
+            uint unum = (uint)num;
+            string hex = "0123456789abcdef";
+            string ans = string.Empty;
+            while (unum != 0)
+            {
+                ans = hex[(int)(unum & 0xf)] + ans;
+                unum >>= 4;
+            }
+
+            return ans;
+        }
+    }
+}
