e099e3e18fa90ce445d35e994d2c43d229eb1f55
gaozeqian <gaozeqian@mingtian.com>
2021-05-28 17:29:13 +0800

    693_交替位二进制数

---
+++ 00-LeetCode/位运算/_693_交替位二进制数.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/binary-number-with-alternating-bits/
+    /// </summary>
+    public class _693_交替位二进制数
+    {
+        public bool HasAlternatingBits(int n)
+        {
+            int last = n % 2;
+            while (n != 0)
+            {
+                n /= 2;
+                int pre = n % 2;
+                if (last == pre)
+                    return false;
+
+                last = pre;
+            }
+
+            return true;
+        }
+    }
+}
