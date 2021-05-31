5e0ce61ebf05b7e9038e7860311ce5972d678e8c
gaozeqian <gaozeqian@mingtian.com>
2021-05-31 18:03:48 +0800

    101_对称二叉树

---
+++ 00-LeetCode/树/_101_对称二叉树.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/symmetric-tree/
+    /// </summary>
+    public class _101_对称二叉树
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+                return true;
+            else
+                return IsSameTree(root.left, root.right);
+        }
+
+        private bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            else if (p == null || q == null)
+                return false;
+            else if (p.val != q.val)
+                return false;
+            else
+                return IsSameTree(p.left, q.right) && IsSameTree(p.right, q.left);
+        }
+    }
+}
