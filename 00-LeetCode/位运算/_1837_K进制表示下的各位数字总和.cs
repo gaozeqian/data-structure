394ecfa998c37b27192f89ef92300b669b835c62
gaozeqian <gaozeqian@mingtian.com>
2021-05-31 12:59:02 +0800

    1837_K进制表示下的各位数字总和

---
+++ 00-LeetCode/位运算/_1837_K进制表示下的各位数字总和.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/sum-of-digits-in-base-k/
+    /// </summary>
+    public class _1837_K进制表示下的各位数字总和
+    {
+        public int SumBase(int n, int k)
+        {
+            int ans = 0;
+            while (n != 0)
+            {
+                ans += n % k;
+                n /= k;
+            }
+
+            return ans;
+        }
+    }
+}
