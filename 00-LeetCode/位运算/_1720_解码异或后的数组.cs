4982674aba923b929856df25afa96f24c7d89248
gaozeqian <gaozeqian@mingtian.com>
2021-05-31 12:44:35 +0800

    1720_解码异或后的数组

---
+++ 00-LeetCode/位运算/_1720_解码异或后的数组.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.位运算
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/decode-xored-array/
+    /// </summary>
+    public class _1720_解码异或后的数组
+    {
+        public int[] Decode(int[] encoded, int first)
+        {
+            int[] arr = new int[encoded.Length + 1];
+            arr[0] = first;
+
+            for (int i = 0; i < encoded.Length; i++)
+                arr[i + 1] = arr[i] ^ encoded[i];
+
+            return arr;
+        }
+    }
+}
