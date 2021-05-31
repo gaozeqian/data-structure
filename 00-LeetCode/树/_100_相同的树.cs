78a4cc14849740e5c73e4ab017a4e02011729a45
gaozeqian <gaozeqian@mingtian.com>
2021-05-31 17:47:15 +0800

    100_相同的树

---
+++ 00-LeetCode/树/_100_相同的树.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/same-tree/
+    /// </summary>
+    public class _100_相同的树
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            else if (p == null || q == null)
+                return false;
+            else if (p.val != q.val)
+                return false;
+            else
+                return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
