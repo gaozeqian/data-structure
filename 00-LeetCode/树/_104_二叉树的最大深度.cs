f5882f157d03c5c46e6adf00f7aa07cc7a4809bd
gaozeqian <gaozeqian@mingtian.com>
2021-06-02 14:44:09 +0800

    104_二叉树的最大深度

---
+++ 00-LeetCode/树/_104_二叉树的最大深度.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+    /// </summary>
+    public class _104_二叉树的最大深度
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            else
+            {
+                int leftDepth = MaxDepth(root.left);
+                int rightDepth = MaxDepth(root.right);
+                return Math.Max(leftDepth, rightDepth) + 1;
+            }
+        }
+    }
+}
