5636d3eab7d0ad5a78871363cca3f33d177fc8c3
gaozeqian <gaozeqian@mingtian.com>
2021-06-03 15:17:44 +0800

    112_路径总和

---
+++ 00-LeetCode/树/_112_路径总和.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/path-sum/
+    /// </summary>
+    public class _112_路径总和
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+                return false;
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while(stack.Count > 0)
+            {
+                root = stack.Pop();
+                if (root.left == null && root.right == null && root.val == targetSum)
+                    return true;
+
+                if(root.left != null)
+                {
+                    root.left.val += root.val;
+                    stack.Push(root.left);
+                }
+
+                if(root.right != null)
+                {
+                    root.right.val += root.val;
+                    stack.Push(root.right);
+                }
+            }
+
+            return false;
+        }
+    }
+}
