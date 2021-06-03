91177410495ade757c7836171dcad65bc525cdc7
gaozeqian <gaozeqian@mingtian.com>
2021-06-03 15:29:46 +0800

    145_二叉树的后序遍历

---
+++ 00-LeetCode/树/_145_二叉树的后序遍历.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+    /// </summary>
+    public class _145_二叉树的后序遍历
+    {
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            List<int> val = new List<int>();
+            Postorder(root, val);
+            return val;
+        }
+
+        private void Postorder(TreeNode root, List<int> val)
+        {
+            if (root == null)
+                return;
+
+            Postorder(root.left, val);
+            Postorder(root.right, val);
+            val.Add(root.val);
+        }
+    }
+}
