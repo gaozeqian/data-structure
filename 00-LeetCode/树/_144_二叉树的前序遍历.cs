4368cfdaa8995d2131b5d428f12f9937ad44b24f
gaozeqian <gaozeqian@mingtian.com>
2021-06-03 15:24:01 +0800

    144_二叉树的前序遍历

---
+++ 00-LeetCode/树/_144_二叉树的前序遍历.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+    /// </summary>
+    public class _144_二叉树的前序遍历
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            List<int> val = new List<int>();
+            Preorder(root, val);
+            return val;
+        }
+
+        private void Preorder(TreeNode root, List<int> val)
+        {
+            if (root == null)
+                return;
+
+            val.Add(root.val);
+            Preorder(root.left, val);
+            Preorder(root.right, val);
+        }
+    }
+}
