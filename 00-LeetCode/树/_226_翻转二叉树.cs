9e7f3e9a526413f3364413d040fc3e898b7ebf17
gaozeqian <gaozeqian@mingtian.com>
2021-06-03 15:41:11 +0800

    226_翻转二叉树

---
+++ 00-LeetCode/树/_226_翻转二叉树.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/invert-binary-tree/
+    /// </summary>
+    public class _226_翻转二叉树
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+
+            TreeNode left = InvertTree(root.left);
+            TreeNode right = InvertTree(root.right);
+            root.left = right;
+            root.right = left;
+
+            return root;
+        }
+    }
+}
