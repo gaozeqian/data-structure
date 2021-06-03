6c8567c5392ce8262d2cc203f09ea91cdb88e867
gaozeqian <gaozeqian@mingtian.com>
2021-06-03 15:02:37 +0800

    111_二叉树的最小深度

---
+++ 00-LeetCode/树/_111_二叉树的最小深度.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
+    /// </summary>
+    public class _111_二叉树的最小深度
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            if (root.left == null && root.right == null)
+                return 1;
+
+            int minDepth = int.MaxValue;
+            if (root.left != null)
+                minDepth = Math.Min(MinDepth(root.left), minDepth);
+
+            if (root.right != null)
+                minDepth = Math.Min(MinDepth(root.right), minDepth);
+
+            return minDepth + 1;
+        }
+    }
+}
