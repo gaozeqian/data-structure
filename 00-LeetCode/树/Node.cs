c4822de2dddc660c4bbf6464c2d8e910f2882a50
gaozeqian <gaozeqian@mingtian.com>
2021-06-23 15:27:00 +0800

    559_N叉树的最大深度

---
+++ 00-LeetCode/树/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    // Definition for a Node.
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
---
+++ 00-LeetCode/树/_559_N叉树的最大深度.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree/
+    /// </summary>
+    public class _559_N叉树的最大深度
+    {
+        public int MaxDepth(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            int maxDepth = 0;
+            foreach (Node node in root.children)
+            {
+                int tempDepth = MaxDepth(node);
+                if (tempDepth > maxDepth)
+                    maxDepth = tempDepth;
+            }
+            return maxDepth + 1;
+        }
+    }
+}
