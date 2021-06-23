4b585bfa3c39ee8943641a5430dc7f40c0673801
gaozeqian <gaozeqian@mingtian.com>
2021-06-23 14:34:56 +0800

    530_二叉搜索树的最小绝对差

---
+++ 00-LeetCode/树/_530_二叉搜索树的最小绝对差.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/
+    /// </summary>
+    public class _530_二叉搜索树的最小绝对差
+    {
+        private int minDiff = int.MaxValue;
+        private int preVal = -1;
+
+        public int GetMinimumDifference(TreeNode root)
+        {
+            Inorder(root);
+            return minDiff;
+        }
+
+        private void Inorder(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            Inorder(root.left);
+            if (preVal >= 0)
+                minDiff = Math.Min(minDiff, Math.Abs(preVal - root.val));
+            preVal = root.val;
+            Inorder(root.right);
+        }
+    }
+}
