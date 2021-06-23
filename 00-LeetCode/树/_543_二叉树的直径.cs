aacb6af67a443b2ce92281e2f4da01f276024fc3
gaozeqian <gaozeqian@mingtian.com>
2021-06-23 15:02:03 +0800

    543_二叉树的直径

---
+++ 00-LeetCode/树/_543_二叉树的直径.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/diameter-of-binary-tree/
+    /// </summary>
+    public class _543_二叉树的直径
+    {
+        private int maxLen = 0;
+
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            Height(root);
+            return maxLen;
+        }
+
+        private int Height(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int leftHeight = Height(root.left);
+            int rightHeight = Height(root.right);
+            maxLen = Math.Max(maxLen, leftHeight + rightHeight);
+            return Math.Max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
