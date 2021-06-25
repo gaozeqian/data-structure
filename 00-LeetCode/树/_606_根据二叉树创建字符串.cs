7dfa854eabc59fcf502e1a4469b1d7f030223d44
gaozeqian <gaozeqian@mingtian.com>
2021-06-25 14:34:53 +0800

    606_根据二叉树创建字符串

---
+++ 00-LeetCode/树/_606_根据二叉树创建字符串.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/construct-string-from-binary-tree/
+    /// </summary>
+    public class _606_根据二叉树创建字符串
+    {
+        private StringBuilder sb = new StringBuilder();
+
+        public string Tree2str(TreeNode root)
+        {
+            Preorder(root);
+            return sb.ToString();
+        }
+
+        private void Preorder(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            sb.Append(root.val);
+
+            if (root.left != null || root.right != null)
+            {
+                sb.Append('(');
+                Preorder(root.left);
+                sb.Append(')');
+            }
+
+            if (root.right != null)
+            {
+                sb.Append('(');
+                Preorder(root.right);
+                sb.Append(')');
+            }
+        }
+    }
+}
