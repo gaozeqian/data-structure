20d5d88a0706f921f2bbd96b072cf0ff8411b9da
gaozeqian <gaozeqian@mingtian.com>
2021-06-25 14:12:39 +0800

    589_N叉树的前序遍历

---
+++ 00-LeetCode/树/_589_N叉树的前序遍历.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _00_LeetCode.树
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/
+    /// </summary>
+    public class _589_N叉树的前序遍历
+    {
+        private List<int> nodes = new List<int>();
+
+        public IList<int> Preorder(Node root)
+        {
+            if (root == null)
+                return nodes;
+
+            nodes.Add(root.val);
+            foreach (Node node in root.children)
+                Preorder(node);
+
+            return nodes;
+        }
+    }
+}
